93ac549a790454586c72c6f2d4720a0d52efc69b
Piyush <impiyush016@gmail.com>
2023-01-07 16:17:40 +0530

    Exercise 1.1

---
+++ part-1/courseinfo/src/App.js
@@ -0,0 +1,69 @@
+const Header = () => {
+  const course = "Half Stack application development";
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  );
+};
+
+const Content = ({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) => {
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </>
+  );
+};
+
+const Total = ({ exercises1, exercises2, exercises3 }) => {
+  return (
+    <>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </>
+  );
+};
+
+const App = () => {
+  const part1 = "Fundamental of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
