2fa669d882f737d99f7f0fe7853ad3551afde890
Piyush <impiyush016@gmail.com>
2023-01-10 10:53:59 +0530

    Exercises 2.4 and 2.5

--- part-2/courseinfo/src/components/Course.js
+++ part-2/courseinfo/src/components/Course.js
@@ -7,43 +7,25 @@ const Course = ({ course }) => {
   );
 };
 
-const Header = ({ course }) => {
-  return (
-    <>
-      <h1>{course.name}</h1>
-    </>
-  );
-};
+const Header = ({ course }) => <h2>{course.name}</h2>;
 
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
 
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-    </>
-  );
-};
+const Content = ({ parts }) =>
+  parts.map((part) => <Part key={part.id} part={part} />);
 
-const Total = ({ parts }) => {
-  return (
-    <>
-      <p>
-        Number of exercises{" "}
-        {parts.reduce((total, part) => {
-          return total + part.exercises;
-        }, 0)}
-      </p>
-    </>
-  );
-};
+const Total = ({ parts }) => (
+  <p style={{ fontWeight: "bold" }}>
+    Total of{" "}
+    {parts.reduce((total, part) => {
+      return total + part.exercises;
+    }, 0)}{" "}
+    exercises
+  </p>
+);
 
 export default Course;
