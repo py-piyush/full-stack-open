62458a9e9d7f6f45c780e160ca58152a7f03f381
Piyush <impiyush016@gmail.com>
2023-01-11 15:40:11 +0530

    Exercise 2.14

--- part-2/countries/src/App.js
+++ part-2/countries/src/App.js
@@ -2,26 +2,76 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const CountryData = ({ country }) => {
+  const [weatherDetails, setWeatherDetails] = useState({});
+  const api_key = process.env.REACT_APP_API_KEY;
   const capital = country.capital[0],
     area = country.area,
     languages = Object.values(country.languages),
     flag = country.flags.svg;
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}`
+      )
+      .then((response) => {
+        const data = response.data;
+        const weather = data.weather[0];
+        const weather_main = weather.main;
+        const weather_desc = weather.description;
+        const weather_icon_url = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+        const temperature = data.main.temp;
+        setWeatherDetails({
+          temperature,
+          weather_main,
+          weather_desc,
+          weather_icon_url,
+        });
+      });
+  }, [api_key, capital]);
+
   return (
-    <div>
-      <h1>{country.name.common}</h1>
-      <h4>Official name: {country.name.official}</h4>
-      <p>Capital: {capital}</p>
-      <p>
-        area: {area.toLocaleString()} km<sup>2</sup>
-      </p>
-      <p>Languages:</p>
-      <ul>
-        {languages.map((language, index) => (
-          <li key={index}>{language}</li>
-        ))}
-      </ul>
-      <img src={flag} alt={country.name.commom} style={{ width: "300px" }} />
-    </div>
+    <>
+      <div>
+        <h1>{country.name.common}</h1>
+        <h4>Official name: {country.name.official}</h4>
+        <p>Capital: {capital}</p>
+        <p>
+          area: {area.toLocaleString()} km<sup>2</sup>
+        </p>
+        <p>Languages:</p>
+        <ul>
+          {languages.map((language, index) => (
+            <li key={index}>{language}</li>
+          ))}
+        </ul>
+        <img src={flag} alt={country.name.commom} style={{ width: "300px" }} />
+      </div>
+      <div>
+        <h2>Weather in {capital}</h2>
+        <p style={{ fontSize: "24px" }}>{weatherDetails.weather_main}</p>
+        <p style={{ marginTop: "-40px" }}>
+          {weatherDetails.weather_desc}{" "}
+          <span>
+            <img
+              src={weatherDetails.weather_icon_url}
+              alt=""
+              style={{
+                width: "50px",
+                height: "50px",
+                position: "relative",
+                top: "20px",
+              }}
+            />
+          </span>
+        </p>
+        <p>
+          temperature:{" "}
+          {Math.round((weatherDetails.temperature - 273.15) * 100) / 100}
+          <sup>o</sup>C
+        </p>
+      </div>
+    </>
   );
 };
 
