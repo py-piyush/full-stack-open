029ed1c90329022fec55e551fe6845eb09436e52
Piyush <impiyush016@gmail.com>
2023-01-17 12:51:51 +0530

    Exercise 3.3

--- part-3/index.js
+++ part-3/index.js
@@ -31,6 +31,13 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = parseInt(request.params.id);
+  const person = persons.find((p) => p.id === id);
+  if (person) response.json(person);
+  else response.status(404).end();
+});
+
 app.get("/info", (req, res) => {
   const info = {
     length: persons.length,
