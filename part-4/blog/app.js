0642948b226331ba9a3e9a388595c288d8735bee
Piyush <impiyush016@gmail.com>
2023-01-20 11:48:23 +0530

    Exercise 4.2: Refactored application

---
+++ part-4/blog/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs.js");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl);
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
--- part-4/blog/index.js
+++ part-4/blog/index.js
@@ -1,20 +1,9 @@
 const http = require("http");
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const Blog = require("./models/blog.js");
-const blogsRouter = require("./controllers/blogs.js");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const app = require("./app.js");
 
-const mongoUrl = process.env.MONGODB_URI;
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-app.use(blogsRouter);
+const server = http.createServer(app);
 
 const PORT = 3003;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
