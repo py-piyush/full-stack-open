3e1dadf7514fe385f96a775bdd2fa43a5e075666
Piyush <impiyush016@gmail.com>
2023-01-20 12:32:54 +0530

    Exercise 4.5

--- part-4/blog/tests/helpers.test.js
+++ part-4/blog/tests/helpers.test.js
@@ -79,3 +79,41 @@ describe("total likes", () => {
     expect(listHelper.totalLikes(blogs)).toBe(36);
   });
 });
+
+describe("favorite blog", () => {
+  test("of empty list is empty object", () => {
+    const blogs = [];
+    expect(listHelper.favoriteBlog(blogs)).toEqual({});
+  });
+
+  test("when list has only one blog equals that blog", () => {
+    const listWithOneBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+    expect(listHelper.favoriteBlog(listWithOneBlog)).toEqual({
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    });
+  });
+  test("of a bigger list is find correctly", () => {
+    expect(listHelper.favoriteBlog(blogs)).toEqual({
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    });
+  });
+});
--- part-4/blog/utils/list_heper.js
+++ part-4/blog/utils/list_heper.js
@@ -4,7 +4,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, item) => total + item.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+  return blogs.reduce(
+    (max, item) => (max.likes >= item.likes ? max : item),
+    blogs[0]
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
