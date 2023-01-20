76a8152111e6e88294ef390083cbebd61288bf3e
Piyush <impiyush016@gmail.com>
2023-01-20 11:38:00 +0530

    Exercise 4.2: Refactored model blog

--- part-4/blog/index.js
+++ part-4/blog/index.js
@@ -2,17 +2,9 @@ const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Blog = require("./models/blog.js");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl);
