76a0cdd419d6499fa7e18c8880a15003280435c7
Piyush <impiyush016@gmail.com>
2023-01-20 12:14:09 +0530

    Exercise 4.3-4.4: starting with testing

---
+++ part-4/blog/utils/list_heper.js
@@ -0,0 +1,10 @@
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, item) => total + item.likes, 0);
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+};
