a2e168b8013b7bbe93480e0e195d52eb2fa6e856
Piyush <impiyush016@gmail.com>
2023-01-27 10:28:44 +0530

    Exercise 4.18: token authentication

--- part-4/blog/app.js
+++ part-4/blog/app.js
@@ -4,6 +4,7 @@ require("express-async-errors");
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs.js");
 const userRouter = require("./controllers/users.js");
+const loginRouter = require("./controllers/login.js");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const middleware = require("./utils/middleware.js");
@@ -15,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
